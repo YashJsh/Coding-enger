b9d13f53d8e00d0b970c476d098615f807aca478
yash <yaxj28@gmail.com>
2026-01-24 12:01:40 +0530

    01-Initialization

---
+++ lib/auth.ts
@@ -0,0 +1,37 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+import { PrismaClient } from "@/lib/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL!;
+const adapter = new PrismaPg({ connectionString });
+
+const prisma = new PrismaClient({
+    adapter
+});
+
+export const auth = betterAuth({
+     emailAndPassword: {
+        enabled: true,
+    },
+    session : {
+        cookieCache : {
+            enabled : true,
+            maxAge : 60,
+        }
+    },
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
+    },
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+});
